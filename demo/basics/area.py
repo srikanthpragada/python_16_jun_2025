8940bc3c92742cf2ef5838949d5a93512fa1602a
srikanthpragada <srikanthpragada@gmail.com>
2025-06-19 16:51:03 +0530

    19-JUN

---
+++ demo/basics/area.py
@@ -0,0 +1,8 @@
+# Accept radius from user and display area of a circle
+
+data = input("Enter radius :")
+radius = float(data)    # Convert str to float
+
+area = 3.14 * radius * radius
+print('Area = ', area)
+
---
+++ demo/basics/wish.py
@@ -0,0 +1,5 @@
+# Take a name from user and display hello
+
+name = input("Enter your name :")
+
+print('Hello', name)
